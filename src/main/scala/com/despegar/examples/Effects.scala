724ed028b8c3a8babcd3f9c2ea2313238b866048
cspinetta <cebs91@gmail.com>
2018-04-29 22:11:35 -0300

    changes on error message

--- src/main/scala/com/despegar/examples/Effects.scala
+++ src/main/scala/com/despegar/examples/Effects.scala
@@ -4,12 +4,12 @@ object Effects {
 
   def divideSideEffect(dividend: Int, divisor: Int): Int = {
     if (divisor == 0)
-      throw new RuntimeException("Divisor mustn't be 0")
+      throw new RuntimeException("Divisor can't be 0")
     dividend / divisor
   }
 
   def divide(dividend: Int, divisor: Int): Either[String, Int] = {
-    if (divisor == 0) Left("Divisor mustn't be 0")
+    if (divisor == 0) Left("Divisor can't be 0")
     else              Right(dividend / divisor)
   }
 
